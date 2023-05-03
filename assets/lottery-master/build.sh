504330cf2ee1bf862d455e89e960dfb53615d556
anxin <884402304@qq.com>
2023-05-03 16:09:54 +0800

    feat(static files): 更新静态文件

--- assets/lottery-master/build.sh
+++
@@ -1,6 +0,0 @@
-TAG=${1:-latest}
-
-rm -f lottery.tar.gz
-tar -czvf lottery.tar.gz ../lottery/
-docker build -t lottery:$TAG  -f ./Dockerfile .
-
--- assets/lottery-master/tagpush.sh
+++
@@ -1,4 +0,0 @@
-TAG=$1
-
-docker tag lottery:latest panda1024/lottery:${TAG}
-docker push panda1024/lottery:${TAG}
